ff6dcf2bd351a1a5a1e605a12d6ee8a45a691ee1
FRDY <rbfridayy@gmail.com>
2023-08-31 20:28:39 +0900

    토큰 인증 자동로그인 구현 => 다양한 상황 테스트 필요

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,62 +4,65 @@ const User = require("../models/User");
 require("dotenv").config();
 
 module.exports = async function (req, res, next) {
-
-  const accessToken = req.cookies.access_token;
-  const refreshToken = req.cookies.refresh_token;
-  const { name } = req.body;
-
   try {
-    // 액세스 토큰이 없으면, 유저 아이디를 db에 검색해 동일한 리프레시 토큰을 갖고 있는 지 확인.
+    let accessToken = req.cookies.access_token;
+    let refreshToken = req.cookies.refresh_token;
+    const { name } = req.body;
+    // 리프레시 토큰이 없으면 로그인 만료 리턴.
+    if (!refreshToken) {
+      await User.updateOne({ name: name }, { $unset: { refresh: 1 } });
+      res.clearCookie("access_token");
+      return res.json({ err: "로그인 정보 만료", state: "expired" });
+    }
+    // 액세스 토큰이 없으면, 유저가 db에 리프레시 토큰을 갖고 있는 지 확인.
     if (!accessToken) {
+      // 리프레시 토큰이 유효한 토큰인지 확인
       const user = await User.findOne({ name: name });
-      // 동일한 리프레시 토큰이라면, 액세스 토큰과 리프레시 토큰 모두 갱신.
-      if (user.refresh === refreshToken) {
-        const payload = {
-          user: {
-            id: user.id,
-          },
-        };
+      if (user) {
+        // 토큰이 유효하고, 유저가 현재 클라이언트와 동일하면 토큰 모두 재발급.
+        if (user.refresh === refreshToken) {
+          const payload = {
+            user: {
+              id: user.id,
+            },
+          };
 
-        const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
-          expiresIn: "15m",
-        });
-        res.cookie("access_token", accessToken, {
-          httpOnly: true,
-          sameSite: "none",
-          domain: "localhost",
-          path: "/",
-          secure: true,
-          maxAge: 15 * 60 * 1000,
-        });
+          accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "15m",
+          });
+          res.cookie("access_token", accessToken, {
+            httpOnly: true,
+            sameSite: "none",
+            domain: "localhost",
+            path: "/",
+            secure: true,
+            maxAge: 15 * 60 * 1000,
+          });
 
-        const refreshToken = jwt.sign(payload, process.env.JWT_SECRET_REFRESH, {
-          expiresIn: "14d",
-        });
+          refreshToken = jwt.sign(payload, process.env.JWT_SECRET_REFRESH, {
+            expiresIn: "14d",
+          });
+          res.cookie("refresh_token", refreshToken, {
+            httpOnly: true,
+            sameSite: "none",
+            domain: "localhost",
+            path: "/",
+            secure: true,
+            maxAge: 14 * 24 * 60 * 60 * 1000,
+          });
 
-        res.cookie("refresh_token", refreshToken, {
-          httpOnly: true,
-          sameSite: "none",
-          domain: "localhost",
-          path: "/",
-          secure: true,
-          maxAge: 14 * 24 * 60 * 60 * 1000,
-        });
-
-        user.refresh = refreshToken;
-        await user.save();
-        // 리프레시 토큰이 만료되었거나 기존 토큰과 다를 시 로그인 정보 만료, 유저db의 refresh 필드 삭제.
-      } else {
-        await User.updateOne({ name: name }, { $unset: { refresh: 1 } });
-        return res.status(401).json({ msg: "로그인 정보가 만료되었습니다." });
+          user.refresh = refreshToken;
+          await user.save();
+        } else {
+          res.clearCookie("refresh_token");
+          res
+            .status(401)
+            .json({ msg: "해당 토큰은 만료되었습니다.", state: "expired" });
+        }
       }
-      return res.status(401).json({ msg: "액세스 토큰이 유효하지 않습니다." });
     }
 
-    // 토큰에 대해 검증.
-
-    // token 해독
-    // token을 만들 때 설정한 secret key 값 : jwtSecret
+    // 토큰 검증.
     const decoded = jwt.verify(accessToken, process.env.JWT_SECRET);
     // req에 해독한 user 정보 생성
     req.user = decoded.user;
